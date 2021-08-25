ab39f1944f8744590872e127f1e30bf927f7dacb
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-08-25 12:43:01 -0300

    Parametros por Referencia

--- 05/index.php
+++ 05/index.php
@@ -1,6 +1,17 @@
 <?php
 /*funcoes em PHP*/
-function somar ($n1,$n2){
+/*n3 está sendo um paramentro nao obrigatório, sendo assim ele possui um valor padrao*/
+/*Aqui os parametros estao sendo passados por valor !!!*/
+function somar ($n1,$n2,$n3=0){
     return $n1+$n2;
 }
-echo somar(3,5);
+echo somar(3,5)."<br>";
+
+/*Passagem de paramentros por REFERENCIA usa-se &$nomeVariavel*/
+$total=5;
+function alteraTotal($n1,$n2,&$total){
+  $total+=$n1+$n2;
+}
+alteraTotal(2,3,$total);
+/*ESSA FUNCAO IRÁ ALTERAR O VALOR DA VARIAVEL QUE ESTA FORA DO ESCOPO POIS REFERENCIA A ELA*/
+echo $total."<br>";
